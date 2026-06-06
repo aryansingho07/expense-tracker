ef4b57d850b95cf31ebef2862528f9faf9062671
Aryan Singh <156529995+aryansingho07@users.noreply.github.com>
2026-06-06 23:20:30 +0530

    Delete backend directory

--- backend/src/constants.js
+++
@@ -1,9 +0,0 @@
-export const CATEGORIES = ["Food", "Transport", "Bills", "Entertainment", "Other"];
-
-export const DEFAULT_BUDGETS = {
-  Food: 0,
-  Transport: 0,
-  Bills: 0,
-  Entertainment: 0,
-  Other: 0
-};
