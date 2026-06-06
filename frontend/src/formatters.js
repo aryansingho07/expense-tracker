4c343bede37b4ff74ae1d5f85072e203cb85a29f
Aryan Singh <156529995+aryansingho07@users.noreply.github.com>
2026-06-06 23:20:19 +0530

    Delete frontend directory

--- frontend/src/formatters.js
+++
@@ -1,9 +0,0 @@
-export const currencyFormatter = new Intl.NumberFormat(undefined, {
-  style: "currency",
-  currency: "INR",
-  maximumFractionDigits: 2
-});
-
-export function formatCurrency(value) {
-  return currencyFormatter.format(value || 0);
-}
